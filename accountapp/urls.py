062a80af768512a7a29e8afc8f66a32c58d72e8d
sunwoo07 <psw8557@gmail.com>
2024-09-14 00:25:00 +0900

    2024_9_14

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from accountapp import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('helloworld/', views.helloworld, name='helloworld'),
+    path('index/', views.index, name='index')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -2,5 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def helloworld(request):
     return HttpResponse('Helloworld')
+
+def index(request):
+    return render(request, 'base.html')
